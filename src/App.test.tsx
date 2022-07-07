6ab71eada6cfd3c502c6e8bae4be2bb9cc3b21e1
Ryan Drumond <ryandrumond@ioasys.com.br>
2022-07-07 00:07:57 -0300

    feat: to do list without react hook form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', async () => {
+test('se ao clicar no botão adicionar com o input preenchido, a task é adicionada à lista', async () => {
+  const expectedString = 'Primeira task'
   render(<App />);
-
-  await screen.findByText(/Teste/i)
+  const input = screen.getByPlaceholderText('Nova tarefa')
+  const button = screen.getByTestId('btnSubmit')
+  userEvent.type(input, expectedString)
+  userEvent.click(button)
+  await screen.findByText(expectedString)
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [taskTitle, setTaskTitle] = useState('')
+  const [taskList, setTaskList] = useState<string[]>([])
+
+  const handleAddTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTaskList(oldList => [...oldList, taskTitle]);
+    setTaskTitle('');
+  }
+
   return (
-    <h1>Ioasys Teste</h1>
+    <>
+      <form onSubmit={handleAddTask}>
+        <input value={taskTitle} onChange={e => setTaskTitle(e.target.value)} placeholder="Nova tarefa" />
+        <button data-testid="btnSubmit" type="submit">Adicionar</button>
+      </form>
+      <ul data-testid="list">
+        {taskList.map(item => <li key={item}>{item}</li>)}
+      </ul>
+    </>
   );
 }
 
